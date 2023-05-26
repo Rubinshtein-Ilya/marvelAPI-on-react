3518681ddebd2247d91f0a2f7741d89c75753c35
Ilya <rubinshtein.ilya@gmail.com>
2023-05-27 02:29:17 +0600

    single comic page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import { MainPage, ComicsPage } from "../pages";
 
 const App = () => {
     
@@ -9,20 +10,24 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                        <Switch>
-                            <Route exact path="/">
-                                <MainPage/>
-                            </Route>
-                            <Route exact path="/comics">
-                                <ComicsPage/>
-                            </Route>
-                        </Switch>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                        <Route exact path="/comics/:comicId">
+                            <SingleComicPage/>
+                        </Route>
+                        <Route path="*">
+                            <Page404/>
+                        </Route>
+                    </Switch>
                 </main>
             </div>
         </Router>
     )
-    
-    
 }
 
 export default App;
